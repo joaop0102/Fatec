f155d9f35f3287318ee956dcc3fbafe85f40ca7a
joaop0102 <joaopedrosilvadeoliveira56@gmail.com>
2025-09-19 13:20:36 -0300

    arrumando algumas atividades

--- TAPjava/AtvdBD/src/main/java/com/mycompany/atvdbd/Usuario.java
+++ TAPjava/AtvdBD/src/main/java/com/mycompany/atvdbd/Usuario.java
@@ -14,14 +14,28 @@ public class Usuario {
     private String nome;
     private String email;
     private int idade;
-    
-    public Usuario(String nome, String email, int idade){
-    this.nome = nome;
-    this.email = email;
-    this.idade = idade;
+
+    public Usuario(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
     @Override
-    public String toString(){
-    return nome + " - " + email + " anos";
+    public String toString() {
+        return "Usuario{nome='" + nome + "', email='" + email + "', idade=" + idade + "}";
     }
 }
+
